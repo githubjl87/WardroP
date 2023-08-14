d88872fc248491d4e0740448a577bf6d30ea7238
Gary Topping <toppinggary@hotmail.com>
2023-08-14 15:07:53 +0900

    seeds new

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,12 @@ User.create!(username: "Ritsuki", address: Faker::Address.full_address, email: "
 User.create!(username: "James", address: Faker::Address.full_address, email: "hkjl87@hotmail.co.uk")
 User.create!(username: "Alvin", address: Faker::Address.full_address, email: "ayaualvin@gmail.com")
 
-Apparel.create!()
+Apparel.create!(name: "skirt", size: "XL", category: "Business", price: 200 , overview: "a nice skirt")
+Apparel.create!(name: "trousers", size: "XL", category: "Business", price: 200 , overview: "a nice trousers")
+Apparel.create!(name: "shirt", size: "XL", category: "Business", price: 200 , overview: "a nice shirt")
+Apparel.create!(name: "top", size: "XL", category: "Business", price: 200 , overview: "a nice top")
+Apparel.create!(name: "bottom", size: "XL", category: "Business", price: 200 , overview: "a nice bottom")
+
+Rental.create!(user_id: 1, cloth_id: 1, start_date: Date.now - 1, end_date: Date.now, status: "accepted")
+
+puts "created #{User.count} users, #{Apparel.count} apparels and #{Rental.count} rentals"
