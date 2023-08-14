5ea10a288a14f5448e6865e4bf59f5601ce174fc
Gary Topping <toppinggary@hotmail.com>
2023-08-14 14:47:07 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "clearing DB"
+
+User.destroy_all
+Review.destroy_all
+Apparel.destroy_all
+Rental.destroy_all
+
+puts "creating DB"
+
+User.create!(username: "Gary", address: Faker::Address.full_address, email: "toppiggary@hotmail.com")
+User.create!(username: "Ritsuki", address: Faker::Address.full_address, email: "ricky0912t@gmail.com")
+User.create!(username: "James", address: Faker::Address.full_address, email: "hkjl87@hotmail.co.uk")
+User.create!(username: "Alvin", address: Faker::Address.full_address, email: "ayaualvin@gmail.com")
+
+Apparel.create!()
