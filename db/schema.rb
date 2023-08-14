1d53b2d02816f91bea9f4409ae6a81096502a60c
Alvin Yau <ayaualvin@gmail.com>
2023-08-14 17:55:07 +0900

    added username and address

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_080147) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
