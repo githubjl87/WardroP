4324e5bd55999e5576a542c77649e2cb2a8c693f
James Lau <hkjl87@hotmail.co.uk>
2023-08-17 14:25:12 +0900

    price updated on index and show

---
+++ app/javascript/controllers/rental_price_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["startDate", "endDate", "price"];
+
+  connect() {}
+
+  update() {
+    const pricePerDay = parseInt(this.priceTarget.dataset.price, 10);
+    if (
+      this.startDateTarget.value !== "" &&
+      this.endDateTarget.value !== ""
+    ) {
+      const diffInMs =
+        new Date(this.endDateTarget.value) -
+        new Date(this.startDateTarget.value);
+      const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+      this.priceTarget.innerText = `${(
+        diffInDays * pricePerDay
+      ).toLocaleString()}`;
+    }
+  }
+}
